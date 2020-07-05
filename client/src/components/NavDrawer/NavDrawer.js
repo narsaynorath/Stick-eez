5a00c32569be18088223880f61fa2da877d92eb3
Nar Saynorath <nar.saynorath@gmail.com>
2020-07-05 10:11:53 -0400

    Make drawer closed size smaller

--- client/src/components/NavDrawer/NavDrawer.js
+++ client/src/components/NavDrawer/NavDrawer.js
@@ -29,7 +29,7 @@ const useStyles = makeStyles(theme => ({
     overflowX: 'hidden',
     width: theme.spacing(7) + 1,
     [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9) + 1,
+      width: theme.spacing(7) + 1,
     },
   },
   drawerPaper: {
